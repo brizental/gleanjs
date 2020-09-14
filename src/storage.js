bf89c992d3480139be5fc3514465f58ed40b6fb5
brizental <beatriz.rizental@gmail.com>
2020-09-14 19:50:15 +0200

    Initial storage implementation
    This relies on localStorage, but webExtensions do not have
    access to that. Instead they have access to a priviledged
    web extensions storag which I don't know much about.
    
    As a follow up to this I should abstract the parts where I talk
    the the underlying storage system, so that we can easily swap
    that out.

---
+++ src/storage.js
@@ -0,0 +1,161 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * Represents the recorded data for a single event.
+ */
+class RecordedEvent {
+    /**
+     * Creates a new RecordedEvent.
+     *
+     * @param {Number} timestamp The timestamp of when the event was recorded. This allows to order events from a single process run.
+     * @param {String} category The event's category. This is defined by users in the metrics file.
+     * @param {String} name The event's name. This is defined by users in the metrics file.
+     * @param {Object} extra A map of all extra data values. The set of allowed extra keys is defined by users in the metrics file.
+     */
+    constructor(timestamp, category, name, extra) {
+        this.timestamp = timestamp;
+        this.category = category;
+        this.name = name;
+        this.extra = extra;
+    }
+
+    /**
+     * Serialize an event to JSON, adjusting its timestamp relative to a base timestamp.
+     *
+     * @param {Number} timestampOffset
+     *
+     * @returns {String} A JSON encoded string representing the serialized event.
+     */
+    serializeRelative(timestampOffset) {
+        return JSON.stringify({
+            timestamp: this.timestamp - timestampOffset,
+            category: this.category,
+            name: this.name,
+            extra: this.extra
+        });
+    }
+}
+
+/**
+ * The key to store glean events on localStorage.
+ */
+const EVENT_STORAGE_KEY = "gleanEvents"
+
+/**
+ * The maximum number of events to hold until it's time to flush.
+ */
+const MAX_EVENTS = 10
+
+/**
+ * The interval in which to batch and send events.
+ */
+const EVENTS_PING_INTERVAL = 60 * 1000; // 5s
+
+class Storage {
+    /**
+     * Creates a new Storage.
+     */
+    constructor() {
+        this._events = this._getPersistedEvents();
+        // The first event we get will be sent immediatelly,
+        // other will be sent when MAX_EVENTS is reached or when we reach the end of an interval.
+        this._atFirstEvent = true;
+        // Set up an interval to send evenst periodically
+        // TODO: Make sure using setInterval is not a terrible idea
+        this._interval = setInterval(this._submitEvents, EVENTS_PING_INTERVAL);
+
+        // If persisted events have reached limit, submit them
+        if (this._events.length >= MAX_EVENTS) {
+            this._submitEvents();
+        }
+    }
+
+    /**
+     * Records a new event in storage.
+     *
+     * @param {Number} timestamp The timestamp of when the event was recorded. This allows to order events from a single process run.
+     * @param {String} category The event's category. This is defined by users in the metrics file.
+     * @param {String} name The event's name. This is defined by users in the metrics file.
+     * @param {Object} extra A map of all extra data values. The set of allowed extra keys is defined by users in the metrics file.
+     */
+    record(timestamp, category, name, extra) {
+        this._pushEvent(new RecordedEvent(timestamp, category, name, extra));
+
+        if (this._atFirstEvent) {
+            this._submitEvents();
+            this._atFirstEvent = false;
+        }
+
+    }
+
+    /**
+     * Submits currently stored events for uploading and clears storage.
+     */
+    _submitEvents() {
+        if (this._events.length > 0) {
+            // TODO: collect / submit code
+
+            this._events = []
+            localStorage.setItem(EVENT_STORAGE_KEY, JSON.stringify(this._events));
+        }
+    }
+
+    /**
+     * Gets a snapshot of the current events.
+     *
+     * @returns {String} A JSON encoded string representing all events stored.
+     */
+    _snapshot() {
+        let snapshot = [];
+        const firstTimestamp = this._events && this._events[0].timestamp;
+        for (const event of this._events) {
+            snapshot.push(event.serializeRelative(firstTimestamp));
+        }
+        return JSON.stringify(snapshot);
+    }
+
+    /**
+     * Adds a new event to `this._events` and triggers ping collection
+     * in case MAX_EVENTS has been reached.
+     *
+     * @param {RecordedEvent} event The event to persist
+     */
+    _pushEvent(event) {
+        this._events.push(event);
+        localStorage.setItem(EVENT_STORAGE_KEY, JSON.stringify(this._events));
+
+        if (this._events.length >= MAX_EVENTS) {
+            this._submitEvents();
+        }
+    }
+
+    /**
+     * Get the persisted events from storage.
+     *
+     * @returns {String[]} The parsed array of events found in localStorage or an empty array.
+     */
+    _getPersistedEvents() {
+        let persisted = localStorage.getItem(EVENT_STORAGE_KEY);
+        if (!persisted) {
+            return [];
+        }
+
+        try {
+            return JSON.parse(persisted);
+        } catch {
+            console.error("Unable to parse Glean events from storage.");
+            localStorage.setItem(EVENT_STORAGE_KEY, JSON.stringify([]));
+            return [];
+        }
+    }
+}
+
+module.exports = {
+    Storage,
+    // Constants exported for testing
+    EVENT_STORAGE_KEY,
+    EVENTS_PING_INTERVAL,
+    MAX_EVENTS
+}
---
+++ tests/storage.test.js
@@ -0,0 +1,156 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const { Storage, ...constants } = require('../src/storage');
+
+afterEach(() => {
+    localStorage.clear();
+});
+
+test('localStorage and _events are kept in sync', () => {
+    const getLocalStorageEvents = () => {
+        const contents = localStorage.getItem(constants.EVENT_STORAGE_KEY);
+        if (contents) {
+            try {
+                return JSON.parse(contents);
+            } catch {
+                throw contents;
+            }
+        } else {
+            return contents;
+        }
+    }
+    const storage = new Storage();
+    const submitSpy = jest.spyOn(storage, "_submitEvents");
+    // Check that initial state is cleared for both storages
+    expect(storage._events.length).toBe(0);
+    expect(getLocalStorageEvents()).toBe(null);
+
+    // Record the first event and be aware that this event will be submitted immediatelly.
+    storage.record(Date.now(), "category", "name", { "extra": "key" });
+    // Check that event was indeed submitted, just to be sure
+    expect(submitSpy).toHaveBeenCalledTimes(1);
+    // Check that stores are both clear after submitting the first event
+    expect(storage._events.length).toBe(0);
+    expect(getLocalStorageEvents().length).toBe(0);
+
+    // Record some events, so taht something is stored
+    storage.record(Date.now(), "category", "name", { "extra": "key" });
+    storage.record(Date.now(), "category", "name", { "extra": "key" });
+    storage.record(Date.now(), "category", "name", { "extra": "key" });
+    // Check that stores are both hold the same amount of events
+    expect(storage._events.length).toBe(3);
+    expect(getLocalStorageEvents().length).toBe(3);
+});
+
+test('submits the first event immediatelly, and not the next ones', () => {
+    const storage = new Storage();
+    const submitSpy = jest.spyOn(storage, "_submitEvents");
+    // Record three events, but only one of this should be immediatelly submitted
+    storage.record(Date.now(), "category", "name", { "extra": "key" });
+    storage.record(Date.now(), "category", "name", { "extra": "key" });
+    storage.record(Date.now(), "category", "name", { "extra": "key" });
+
+    // Check the ping was submitted
+    expect(submitSpy).toHaveBeenCalledTimes(1);
+    // Check that event submitted event is not in storage anymore
+    expect(storage._events.length).toBe(2);
+});
+
+test('submits events when interval is reached, if there are events', () => {
+    // Stub the interval constant, so that we don't have to wait too long on this test.
+    constants.EVENTS_PING_INTERVAL = 3 * 1000; // 3s
+
+    const storage = new Storage();
+    // Record the first event before setting up the spy,
+    // because it will trigger a _submitEvents that we don't care about
+    storage.record(Date.now(), "category", "name", { "extra": "key" });
+
+    const submitSpy = jest.spyOn(storage, "_submitEvents");
+    // Record some events, so that we have something to submit when it is time
+    storage.record(Date.now(), "category", "name", { "extra": "key" });
+    storage.record(Date.now(), "category", "name", { "extra": "key" });
+    storage.record(Date.now(), "category", "name", { "extra": "key" });
+
+    // Check that nothing was submitted yet, we haven't reached the end of the interval
+    expect(submitSpy).toHaveBeenCalledTimes(0);
+    // All events should still be in storage
+    expect(storage._events.length).toBe(3);
+    setTimeout(() => {
+        // Check that ping was submitted
+
+        // Check that events storage was cleared
+        expect(storage._events.length).toBe(0);
+    }, constants.EVENTS_PING_INTERVAL)
+});
+
+test('submits events when limit is reached', () => {
+    const storage = new Storage();
+    // Record the first event before setting up the spy,
+    // because it will trigger a _submitEvents that we don't care about
+    storage.record(Date.now(), "category", "name", { "extra": "key" });
+
+    const submitSpy = jest.spyOn(storage, "_submitEvents");
+    // Submit the max number of events to trigger submission
+    for (let i = 0; i < constants.MAX_EVENTS; i++) {
+        storage.record(Date.now(), "category", "name", { "extra": "key" });
+    }
+
+    // Check that ping was submitted
+    expect(submitSpy).toHaveBeenCalledTimes(1);
+    // Check that events storage was cleared
+    expect(storage._events.length).toBe(0);
+});
+
+test('gets persisted events on init', () => {
+    const storage1 = new Storage();
+    // Record the first event and be aware that this event will be submitted immediatelly.
+    storage1.record(Date.now(), "category", "name", { "extra": "key" });
+
+    // Record some events, so that we have something to persist when it is time
+    storage1.record(Date.now(), "category", "name", { "extra": "key" });
+    storage1.record(Date.now(), "category", "name", { "extra": "key" });
+    storage1.record(Date.now(), "category", "name", { "extra": "key" });
+
+    const storage2 = new Storage();
+    const submitSpy = jest.spyOn(storage2, "_submitEvents");
+    // Check that nothing was submitted, we are not at max
+    expect(submitSpy).toHaveBeenCalledTimes(0);
+    // Check that persisted events were loaded to _events
+    expect(storage2._events.length).toBe(3);
+});
+
+test('submits persisted events when they are above max', () => {
+    const storage1 = new Storage();
+    // Mock the _submitEvent call so that it doesn't clear anything
+    storage1._submitEvents = () => {}
+    // Submit the max number of events to trigger submission
+    for (let i = 0; i < constants.MAX_EVENTS; i++) {
+        storage1.record(Date.now(), "category", "name", { "extra": "key" });
+    }
+
+    const storage2 = new Storage();
+    const submitSpy = jest.spyOn(storage2, "_submitEvents");
+    // Check that events were indeed submitted since they were above max
+    expect(submitSpy).toHaveBeenCalledTimes(0);
+    // Check that persisted events were cleared
+    expect(storage2._events.length).toBe(0);
+});
+
+test('correctly snapshots the storage', () => {
+    const storage = new Storage();
+    // Record the first event and be aware that this event will be submitted immediatelly.
+    storage.record(Date.now(), "category", "name", { "extra": "key" });
+
+    // Record some events, so that we have something to persist when it is time
+    storage.record(Date.now(), "category", "name", { "extra": "key" });
+    storage.record(Date.now(), "category", "name", { "extra": "key" });
+    storage.record(Date.now(), "category", "name", { "extra": "key" });
+
+    expect(storage._snapshot()).toBe("[\"{\\\"timestamp\\\":0,\\\"category\\\":\\\"category\\\",\\\"name\\\":\\\"name\\\",\\\"extra\\\":{\\\"extra\\\":\\\"key\\\"}}\",\"{\\\"timestamp\\\":0,\\\"category\\\":\\\"category\\\",\\\"name\\\":\\\"name\\\",\\\"extra\\\":{\\\"extra\\\":\\\"key\\\"}}\",\"{\\\"timestamp\\\":0,\\\"category\\\":\\\"category\\\",\\\"name\\\":\\\"name\\\",\\\"extra\\\":{\\\"extra\\\":\\\"key\\\"}}\"]");
+});
+
+
+
+
