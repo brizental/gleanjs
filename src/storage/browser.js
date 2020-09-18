8b142abbefbfd1d229d980d653b3cfcdf39139eb
brizental <beatriz.rizental@gmail.com>
2020-09-18 12:41:13 +0200

    Attend to review comments

--- src/storage/browser.js
+++ src/storage/browser.js
@@ -2,6 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/**
+ * Interface for the localStorage,
+ * a key-value store that is available on browser environments.
+ */
+
 module.exports = {
     getItem(key) {
         return localStorage.getItem(key);
