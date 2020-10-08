fdf4eb979604b5628c040bb36e5df658a57a4edf
Beatriz Rizental <beatriz.rizental@gmail.com>
2020-10-08 17:23:20 +0200

    Update src/ping_maker/index.js
    Co-authored-by: Alessio Placitelli <alessio.placitelli@gmail.com>

--- src/ping_maker/index.js
+++ src/ping_maker/index.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
- const {
+const {
     PENDING_PINGS_STORAGE_KEY,
     CLIENT_ID_KEY,
     FIRST_RUN_DATE_KEY,
